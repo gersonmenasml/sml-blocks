25f77e89af6b2497b75864476a47b134803b9c5f
Adrienne <adriennecallie.ng@gmail.com>
2021-12-20 10:43:32 +1100

    Fix bugs: gallery component

--- template-parts/blocks/gallery/gallery.php
+++ template-parts/blocks/gallery/gallery.php
@@ -32,17 +32,39 @@ $description = get_field('description') ?: 'Text here...';
 $description2 = get_field('description2') ?: 'Text here...';
 $image = get_field('image');
 $image2 = get_field('image2');
+$button_text = get_field('button_text');
+$button_text_2 = get_field('button_text_2');
 
 ?>
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
 
             
 <div class="min-h-screen  flex flex-wrap items-center justify-start">
-        <div class="mr-0 lg:mr-0 -space-y-16 md:py-24 px-40 md:px-80 w-full md:max-w-min sm:w-full bg-blue-200 transform scale-1 sm:scale-1 md:scale-105 lg:scale-105 xl:scale-105 z-40  shadow-none sm:shadow-none md:shadow-xl lg:shadow-xl xl:shadow-xl">
+        <div class="mr-0 lg:mr-0 -space-y-16 md:py-0 md:max-w-max 5 z-40  shadow-none sm:shadow-none  lg:shadow-xl xl:shadow-xl mr-0 lg:mr-80">
             <img src="<?php echo $image; ?>" alt="">
         </div>
 
-        <div class="py-6 sm:py-8 px-10 w-full md:max-w-min sm:w-full bg-white z-50  shadow-none sm:shadow-none md:shadow-xl lg:shadow-xl xl:shadow-xl">
+         <div class="visible md:invisible lg:hidden py-6 sm:py-6 md:py-6 lg:py-6 xl:py-4 px-8 w-full md:max-w-min sm:w-full bg-white z-50">
+
+            <div class="text-left py-4 px-14">
+                <h1 class="text-gray-700 text-4xl font-black mr-0 lg:mr-2 text-center"><?php echo $title; ?></h1>
+            </div>
+
+            <div class="h-px bg-gray-200"></div>
+
+            <div class="text-center mt-3">
+                <p class="text-sm text-gray-400">
+                <?php echo $description; ?>
+                </p>
+            </div>
+
+            <button class="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800"><?php echo $button_text; ?></button>
+
+        </div>
+
+         <!-- Mobile version for our people section -->
+
+        <div class="invisible md:visible py-6 sm:py-8 px-10 w-full md:max-w-min sm:w-full bg-white z-50  shadow-none sm:shadow-none md:shadow-xl lg:shadow-xl xl:shadow-xl relative md:absolute right-20">
 
             <div class="text-left py-4 px-14">
                 <h1 class="text-gray-700 text-4xl text-center font-black"><?php echo $title; ?></h1>
@@ -56,7 +78,7 @@ $image2 = get_field('image2');
                 </p>
             </div>
 
-            <button class="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800">Learn More</button>
+            <button class="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800"><?php echo $button_text; ?></button>
 
         </div>
     </div>
@@ -67,7 +89,7 @@ $image2 = get_field('image2');
 
     <div class="min-h-screen flex flex-wrap items-center justify-end">
 
-        <div class="invisible md:visible -space-y-20 md:space-y-6 py-8 px-10 w-full md:max-w-min sm:w-full bg-white z-50  shadow-none sm:shadow-none md:shadow-xl lg:shadow-xl xl:shadow-xl">
+        <div class="invisible md:visible -space-y-80 md:space-y-6 py-6 px-10 w-full md:max-w-min sm:w-full bg-white z-50  shadow-none sm:shadow-none md:shadow-xl lg:shadow-xl xl:shadow-xl relative md:absolute left-20">
 
             <div class="text-left py-4 px-14">
                 <h1 class="text-gray-700 text-4xl font-black text-center"><?php echo $title2; ?></h1>
@@ -81,12 +103,12 @@ $image2 = get_field('image2');
                 </p>
             </div>
 
-            <button class="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800">Learn More</button>
+            <button class="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800"><?php echo $button_text_2; ?></button>
 
         </div>
 
-        <div class="mr-0 lg:mr-0 -space-y-16 md:py-24 px-80 w-full md:max-w-min sm:w-full bg-blue-200 transform scale-1 sm:scale-1 md:scale-105 lg:scale-105 xl:scale-105 z-40  shadow-none sm:shadow-none md:shadow-xl lg:shadow-xl xl:shadow-xl">
-        <img src="<?php echo $image2; ?>" alt="">
+        <div class="mr-0 lg:mr-0 -space-y-16 md:py-0 md:max-w-max 5 z-40  shadow-none sm:shadow-none  lg:shadow-xl xl:shadow-xl ml-0 lg:ml-80">
+            <img src="<?php echo $image2; ?>" alt="">
         </div>
 
         <!-- Mobile version for our community section -->
@@ -94,18 +116,18 @@ $image2 = get_field('image2');
         <div class="visible md:invisible lg:hidden py-6 sm:py-6 md:py-6 lg:py-6 xl:py-4 px-8 w-full md:max-w-min sm:w-full bg-white z-50">
 
             <div class="text-left py-4 px-14">
-                <h1 class="text-gray-700 text-4xl font-black mr-0 lg:mr-2 text-center">Our Community</h1>
+                <h1 class="text-gray-700 text-4xl font-black mr-0 lg:mr-2 text-center"><?php echo $title2; ?></h1>
             </div>
 
             <div class="h-px bg-gray-200"></div>
 
             <div class="text-center mt-3">
                 <p class="text-sm text-gray-400">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                <?php echo $description2; ?>
                 </p>
             </div>
 
-            <button class="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800">Learn More</button>
+            <button class="w-full mt-6 mb-3 py-2 text-white font-semibold bg-gray-700 hover:shadow-xl duration-200 hover:bg-gray-800"><?php echo $button_text_2; ?></button>
 
         </div>
 
